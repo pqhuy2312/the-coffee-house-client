2f4b60d1d6dcb2108c5cd0dd3bdb4f291361eea2
pqhuy2312 <pqhuy2312@gmail.com>
2022-06-06 11:29:22 +0700

    first commit

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const withPlugins = require("next-compose-plugins");
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+module.exports = withPlugins([[withBundleAnalyzer]], {
+  images: {
+    formats: ["image/avif", "image/webp"],
+    domains: ["res.cloudinary.com"],
+  },
+});
